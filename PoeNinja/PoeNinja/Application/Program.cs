318b787a4fc1f76d7969a0cefdb6b28959eb8181
Yaroslav Lazakovich <yaroslav.lazakovich@idt.net>
2019-11-09 14:49:23 +0300

    commit from 2-nd PC;

--- PoeNinja/PoeNinja/Application/Program.cs
+++ PoeNinja/PoeNinja/Application/Program.cs
@@ -29,7 +29,6 @@ namespace PoeNinja.Application
             vault.SetJewelVault(client);
 
             TakeDataFromVault(vault);
-            Console.WriteLine("check-actual-branch");
         }
     }
 }
